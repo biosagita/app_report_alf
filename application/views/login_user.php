bbbbc26170bbaab8f6a1fc376ab6300daf733d3f
Jey Developer <jey.developer@google.com>
2020-05-13 21:55:49 +0700

    update login header

---
+++ application/views/login_user.php
@@ -0,0 +1,3 @@
+<div id="page-title" style="text-align: right;">
+    <h4>Welcome <?php echo $this->session->userdata('nama')?>, <a href="<?php echo site_url('login/logout')?>">logout</a></h4>
+</div>
