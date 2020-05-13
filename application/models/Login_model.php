fd3919bf6d14e74eca7c44211a4cba5cf5293e69
Jey Developer <jey.developer@google.com>
2020-05-13 17:13:14 +0700

    update login

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,23 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login_model extends CI_Model {
+class Login_model extends CI_Model
+{
+    public function m_register()
+    {
+        $data = array('nama' => $this->input->post('nama'),
+            'email' => $this->input->post('email'),
+            'password' => get_hash($this->input->post('password')));
+        return $this->db->insert('user', $data);
+    }
 
-
-	public function m_register() {
-
-        $data = array('nama' =>$this->input->post('nama'),
-                      'email'=>$this->input->post('email'),
-                      'password'=>get_hash($this->input->post('password')));
-
-        return $this->db->insert('user',$data);
-
-	}
-
-     public function m_cek_user() {
-
-     return $this->db->get_where('user',array('nama' => $this->input->post('user')));
-
-     }	
-
+    public function m_cek_user()
+    {
+        return $this->db->get_where('user', array('nama' => $this->input->post('user'), 'password' => MD5($this->input->post('password'))));
+    }
 }
